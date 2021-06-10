fa8ba45985feda20e1a3f3cb7e56794b2f4bbd55
Sanchit Sinha <68417985+sanchit-sinha@users.noreply.github.com>
2021-06-10 11:33:56 +0530

    Login logout api (#1)
    * added login
    
    * add logout view
    
    * update
    
    * add password hash
    
    * update login api
    
    * update
    
    * update

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.LoginView ,  name="auth-login")
+    path("login/", views.Login.as_view(), name="auth-login"),
+    path("logout/", views.Logout.as_view(), name="auth-logout"),
 ]
